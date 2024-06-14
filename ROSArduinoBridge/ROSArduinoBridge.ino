6394f07ef90008a741ab103b61bdd1929d7dd60d
joshua.krafchin <joshua.krafchin@gmail.com>
2024-06-14 16:44:07 -0700

    Motor turns on but encoder not yet working

--- ROSArduinoBridge/ROSArduinoBridge.ino
+++ ROSArduinoBridge/ROSArduinoBridge.ino
@@ -1,69 +1,13 @@
-/*********************************************************************
- *  ROSArduinoBridge
- 
-    A set of simple serial commands to control a differential drive
-    robot and receive back sensor and odometry data. Default 
-    configuration assumes use of an Arduino Mega + Pololu motor
-    controller shield + Robogaia Mega Encoder shield.  Edit the
-    readEncoder() and setMotorSpeed() wrapper functions if using 
-    different motor controller or encoder method.
-
-    Created for the Pi Robot Project: http://www.pirobot.org
-    and the Home Brew Robotics Club (HBRC): http://hbrobotics.org
-    
-    Authors: Patrick Goebel, James Nugen
-
-    Inspired and modeled after the ArbotiX driver by Michael Ferguson
-    
-    Software License Agreement (BSD License)
-
-    Copyright (c) 2012, Patrick Goebel.
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above
-       copyright notice, this list of conditions and the following
-       disclaimer in the documentation and/or other materials provided
-       with the distribution.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
 #define USE_BASE      // Enable the base controller code
 //#undef USE_BASE     // Disable the base controller code
 
 /* Define the motor controller and encoder library you are using */
 #ifdef USE_BASE
-   /* The Pololu VNH5019 dual motor driver shield */
-   //#define POLOLU_VNH5019
+   /* L298 Motor driver */
+   #define L298_MOTOR_DRIVER
 
-   /* The Pololu MC33926 dual motor driver shield */
-   //#define POLOLU_MC33926
-
-   /* The RoboGaia encoder shield */
-   //#define ROBOGAIA
-   
    /* Encoders directly attached to Arduino board */
    #define ARDUINO_ENC_COUNTER
-
-   /* L298 Motor driver*/
-   #define L298_MOTOR_DRIVER
 #endif
 
 //#define USE_SERVOS  // Enable use of PWM servos as defined in servos.h
@@ -80,6 +24,8 @@
 #else
 #include "WProgram.h"
 #endif
+
+#include <avr/io.h>
 
 /* Include definition of serial commands */
 #include "commands.h"
@@ -114,13 +60,13 @@
 
   /* Stop the robot if it hasn't received a movement command
    in this number of milliseconds */
-  #define AUTO_STOP_INTERVAL 2000
+  #define AUTO_STOP_INTERVAL 10000
   long lastMotorCommand = AUTO_STOP_INTERVAL;
 #endif
 
 /* Variable initialization */
 
-// A pair of varibles to help parse serial commands (thanks Fergs)
+// A pair of variables to help parse serial commands (thanks Fergs)
 int arg = 0;
 int index = 0;
 
@@ -137,6 +83,16 @@ char argv2[16];
 // The arguments converted to integers
 long arg1;
 long arg2;
+
+/* Encoder variables */
+volatile long leftEncoderTicks = 0;
+volatile long rightEncoderTicks = 0;
+
+// Pin definitions for encoders
+#define LEFT_ENC_PIN_A 2
+#define LEFT_ENC_PIN_B 3
+#define RIGHT_ENC_PIN_A 4
+#define RIGHT_ENC_PIN_B 5
 
 /* Clear the current command parameters */
 void resetCommand() {
@@ -157,6 +113,13 @@ int runCommand() {
   int pid_args[4];
   arg1 = atoi(argv1);
   arg2 = atoi(argv2);
+  
+  Serial.print("Command: ");
+  Serial.println(cmd);
+  Serial.print("Arg1: ");
+  Serial.println(arg1);
+  Serial.print("Arg2: ");
+  Serial.println(arg2);
   
   switch(cmd) {
   case GET_BAUDRATE:
@@ -185,7 +148,7 @@ int runCommand() {
   case PING:
     Serial.println(Ping(arg1));
     break;
-#ifdef USE_SERVOS
+  #ifdef USE_SERVOS
   case SERVO_WRITE:
     servos[arg1].setTargetPosition(arg2);
     Serial.println("OK");
@@ -193,16 +156,17 @@ int runCommand() {
   case SERVO_READ:
     Serial.println(servos[arg1].getServo().read());
     break;
-#endif
+  #endif
     
-#ifdef USE_BASE
+  #ifdef USE_BASE
   case READ_ENCODERS:
-    Serial.print(readEncoder(LEFT));
+    Serial.print(leftEncoderTicks);
     Serial.print(" ");
-    Serial.println(readEncoder(RIGHT));
+    Serial.println(rightEncoderTicks);
     break;
    case RESET_ENCODERS:
-    resetEncoders();
+    leftEncoderTicks = 0;
+    rightEncoderTicks = 0;
     resetPID();
     Serial.println("OK");
     break;
@@ -238,45 +202,79 @@ int runCommand() {
     Ko = pid_args[3];
     Serial.println("OK");
     break;
-#endif
+  #endif
   default:
     Serial.println("Invalid Command");
     break;
   }
 }
 
+/* Interrupt service routines for encoders */
+void handleLeftEncA() {
+  if (digitalRead(LEFT_ENC_PIN_A) == digitalRead(LEFT_ENC_PIN_B)) {
+    leftEncoderTicks++;
+  } else {
+    leftEncoderTicks--;
+  }
+  Serial.print("Left Encoder Ticks: ");
+  Serial.println(leftEncoderTicks);
+}
+
+void handleLeftEncB() {
+  if (digitalRead(LEFT_ENC_PIN_A) == digitalRead(LEFT_ENC_PIN_B)) {
+    leftEncoderTicks--;
+  } else {
+    leftEncoderTicks++;
+  }
+  Serial.print("Left Encoder Ticks: ");
+  Serial.println(leftEncoderTicks);
+}
+
+void handleRightEncA() {
+  if (digitalRead(RIGHT_ENC_PIN_A) == digitalRead(RIGHT_ENC_PIN_B)) {
+    rightEncoderTicks++;
+  } else {
+    rightEncoderTicks--;
+  }
+  Serial.print("Right Encoder Ticks: ");
+  Serial.println(rightEncoderTicks);
+}
+
+void handleRightEncB() {
+  if (digitalRead(RIGHT_ENC_PIN_A) == digitalRead(RIGHT_ENC_PIN_B)) {
+    rightEncoderTicks--;
+  } else {
+    rightEncoderTicks++;
+  }
+  Serial.print("Right Encoder Ticks: ");
+  Serial.println(rightEncoderTicks);
+}
+
 /* Setup function--runs once at startup. */
 void setup() {
   Serial.begin(BAUDRATE);
+  Serial.println("Motor controller test starting...");
 
-// Initialize the motor controller if used */
-#ifdef USE_BASE
-  #ifdef ARDUINO_ENC_COUNTER
-    //set as inputs
-    DDRD &= ~(1<<LEFT_ENC_PIN_A);
-    DDRD &= ~(1<<LEFT_ENC_PIN_B);
-    DDRC &= ~(1<<RIGHT_ENC_PIN_A);
-    DDRC &= ~(1<<RIGHT_ENC_PIN_B);
-    
-    //enable pull up resistors
-    PORTD |= (1<<LEFT_ENC_PIN_A);
-    PORTD |= (1<<LEFT_ENC_PIN_B);
-    PORTC |= (1<<RIGHT_ENC_PIN_A);
-    PORTC |= (1<<RIGHT_ENC_PIN_B);
-    
-    // tell pin change mask to listen to left encoder pins
-    PCMSK2 |= (1 << LEFT_ENC_PIN_A)|(1 << LEFT_ENC_PIN_B);
-    // tell pin change mask to listen to right encoder pins
-    PCMSK1 |= (1 << RIGHT_ENC_PIN_A)|(1 << RIGHT_ENC_PIN_B);
-    
-    // enable PCINT1 and PCINT2 interrupt in the general interrupt mask
-    PCICR |= (1 << PCIE1) | (1 << PCIE2);
+  // Initialize the motor controller if used
+  #ifdef USE_BASE
+    #ifdef ARDUINO_ENC_COUNTER
+      // Set as inputs
+      pinMode(LEFT_ENC_PIN_A, INPUT_PULLUP);
+      pinMode(LEFT_ENC_PIN_B, INPUT_PULLUP);
+      pinMode(RIGHT_ENC_PIN_A, INPUT_PULLUP);
+      pinMode(RIGHT_ENC_PIN_B, INPUT_PULLUP);
+      
+      // Enable pin change interrupts
+      attachInterrupt(digitalPinToInterrupt(LEFT_ENC_PIN_A), handleLeftEncA, CHANGE);
+      attachInterrupt(digitalPinToInterrupt(LEFT_ENC_PIN_B), handleLeftEncB, CHANGE);
+      attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_PIN_A), handleRightEncA, CHANGE);
+      attachInterrupt(digitalPinToInterrupt(RIGHT_ENC_PIN_B), handleRightEncB, CHANGE);
+    #endif
+    initMotorController();
+    resetPID();
   #endif
-  initMotorController();
-  resetPID();
-#endif
 
-/* Attach servos if used */
+  // Attach servos if used
   #ifdef USE_SERVOS
     int i;
     for (i = 0; i < N_SERVOS; i++) {
@@ -286,15 +284,12 @@ void setup() {
           servoInitPosition[i]);
     }
   #endif
+
+  Serial.println("Setup complete.");
 }
 
-/* Enter the main loop.  Read and parse input from the serial port
-   and run any valid commands. Run a PID calculation at the target
-   interval and check for auto-stop conditions.
-*/
 void loop() {
   while (Serial.available() > 0) {
-    
     // Read the next character
     chr = Serial.read();
 
@@ -302,6 +297,7 @@ void loop() {
     if (chr == 13) {
       if (arg == 1) argv1[index] = NULL;
       else if (arg == 2) argv2[index] = NULL;
+      Serial.println("Running command...");
       runCommand();
       resetCommand();
     }
@@ -333,26 +329,25 @@ void loop() {
     }
   }
   
-// If we are using base control, run a PID calculation at the appropriate intervals
-#ifdef USE_BASE
-  if (millis() > nextPID) {
-    updatePID();
-    nextPID += PID_INTERVAL;
-  }
-  
-  // Check to see if we have exceeded the auto-stop interval
-  if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {;
-    setMotorSpeeds(0, 0);
-    moving = 0;
-  }
-#endif
+  // If we are using base control, run a PID calculation at the appropriate intervals
+  #ifdef USE_BASE
+    if (millis() > nextPID) {
+      updatePID();
+      nextPID += PID_INTERVAL;
+    }
+    
+    // Check to see if we have exceeded the auto-stop interval
+    if ((millis() - lastMotorCommand) > AUTO_STOP_INTERVAL) {
+      setMotorSpeeds(0, 0);
+      moving = 0;
+    }
+  #endif
 
-// Sweep servos
-#ifdef USE_SERVOS
-  int i;
-  for (i = 0; i < N_SERVOS; i++) {
-    servos[i].doSweep();
-  }
-#endif
+  // Sweep servos
+  #ifdef USE_SERVOS
+    int i;
+    for (i = 0; i < N_SERVOS; i++) {
+      servos[i].doSweep();
+    }
+  #endif
 }
-
--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -4,17 +4,15 @@
    
    
 #ifdef ARDUINO_ENC_COUNTER
-  //below can be changed, but should be PORTD pins; 
-  //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A PD2  //pin 2
-  #define LEFT_ENC_PIN_B PD3  //pin 3
+  // All pins are part of PORTD in your configuration
+  #define LEFT_ENC_PIN_A PD2  // Arduino pin D5
+  #define LEFT_ENC_PIN_B PD3  // Arduino pin D6
   
-  //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A PC4  //pin A4
-  #define RIGHT_ENC_PIN_B PC5   //pin A5
+  // Adjusting the other encoder pins to use PORTD as well
+  #define RIGHT_ENC_PIN_A PC4  // Arduino pin D9
+  #define RIGHT_ENC_PIN_B PC5 // Arduino pin D10
 #endif
    
 long readEncoder(int i);
 void resetEncoder(int i);
 void resetEncoders();
-
